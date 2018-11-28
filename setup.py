a3802c8a084c58491a16b99a1f96ad64c696f716
LyricLy <gulliverhanson@gmail.com>
2018-11-28 13:44:27 +1300

    Add PyPI

---
+++ setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name = "capncord.py",
+    packages = setuptools.find_packages(),
+    version = "0.1.0",
+    description = "A wrapper for the Capncord API.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    author = "LyricLy",
+    author_email = "gulliverhanson@gmail.com",
+    url = "https://github.com/LyricLy/capncord.py",
+    keywords = ["wrapper", "capncord", "capncord.py"],
+    install_requires = requirements,
+    license = "CC0",
+    classifiers = [
+            "Development Status :: 3 - Alpha",
+            "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+            "Intended Audience :: Developers",
+            "Natural Language :: English",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 3.5",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3 :: Only",
+            "Topic :: Internet",
+            "Topic :: Software Development :: Libraries",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+            "Topic :: Utilities"
+    ]
+)
